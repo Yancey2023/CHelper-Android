924fb2f64f62b375ec38d8effc9c5c8541bac38a
Yancey <1709185482@qq.com>
2024-12-10 13:03:24 +0800

    update version 0.2.33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "yancey.chelper"
         minSdk = 24
         targetSdk = 34
-        versionCode = 45
-        versionName = "0.2.32-beta"
+        versionCode = 46
+        versionName = "0.2.33-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -63,7 +63,7 @@ android {
         viewBinding = true
     }
 
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.2.12479018"
 
     kotlinOptions {
         jvmTarget = "17"
@@ -101,9 +101,9 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
     // https://github.com/boxbeam/Crunch
     implementation("com.github.Redempt:Crunch:2.0.3")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
